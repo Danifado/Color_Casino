f8fd4dbe8f7b53f77d92039983b4c7714d67f5d3
victorsicacha <55368819+victorsicacha@users.noreply.github.com>
2020-12-02 15:41:31 -0500

    Add files via upload

--- colorCasinoBackend/api/controllers/jugar.js
+++ colorCasinoBackend/api/controllers/jugar.js
@@ -3,16 +3,37 @@
 let bcrypt = require('bcrypt-node');
 const {param} = require('../app');
 let Maquina = require('../models/maquina');
+let User=require('../models/User')
 let jwt = require('../services/jwt');
 let mongoosePaginate = require('mongoose-pagination');
 
 function apostar(req, res){
     res.status(200).send(
     {saludo: "aquí deberiamos verificar la cuenta del usuario"}
+    Maquina.find().paginate(page, docsPerPage, (err, Maquinas, total) => {
+        if(err){
+            return res.status(500).send({
+                message: 'Hubo un error consultando las maquinas'
+            })
+        }
+
+        if(!Maquinas) return res.status(200).send({
+            message: "No hay maquinas"
+        })
+
+        return res.status(200).send({
+            tipo,
+            token,
+            usos:usos+1
+        })
+
+    })
 )}
 
-function pruebas(req, res){
+function prueba(req, res){
     res.status(200).send(
         {saludo: "pues si, si funciona"}
     )
 }
+
+
